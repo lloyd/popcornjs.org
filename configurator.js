1d53251fbcfb991bf260b9bd3c1cf8247d6441ab
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-09-01 10:58:05 +0300

    fetch json manifest and populate available elements using data from within it

--- configurator.js
+++ configurator.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
   $.get("code/dist/manifest.json", function (mani) {
-    alert(mani);
-  });
+    // populate all of the choices
+    for (var k in mani) {
+      if (!mani.hasOwnProperty(k)) continue;
+
+      // first remove the ... place holder
+      $("#elements > ul." + k + " li:nth-child(2)").remove();
+
+      // now add all the items
+      var parent = $("#elements > ul." + k);
+      for (var i = 0; i < mani[k].length; i++) {
+        var n = $("<div><span class='name'></span></div>");
+        var shortname = mani[k][i].replace(/^popcorn./, "").replace(/\.js$/, "");
+        n.find(".name").text(shortname).attr("file", mani[k][i]);
+        parent.append(n);
+      }
+
+    }
+  }, 'json');
 });
